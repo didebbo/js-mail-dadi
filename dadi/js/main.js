599596ed001a1bd2e75ef3a59c3a9238c5e635d7
didebbo <gianluca.napoletano.93@gmail.com>
2021-07-30 15:51:58 +0200

    aggiunta logica a dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,7 +1,7 @@
 var user = Math.floor(Math.random() * 6) + 1;
 var cpu = Math.floor(Math.random() * 6) + 1;
 
-alert("Tu:" + user + " CPU: " + cpu);
+alert("Tu: " + user + " CPU: " + cpu);
 
 if (user > cpu) alert("Tu hai vinto!");
 else if (user < cpu) alert("CPU ha vinto!");
